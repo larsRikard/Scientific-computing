3114df0ae1b1e911d1282b765dcb150c0d508e6b
Rekkert <lars.r.raadstoga@gmail.com>
2021-08-25 22:42:52 +0200

    worked on assignment

--- Assignment/linear_interpolation.py
+++ Assignment/linear_interpolation.py
@@ -9,16 +9,29 @@ import matplotlib.pyplot as plt
 # t_3 <= 3.2 <= t_4
 
 # part 2.
-# A mathematical expression of a line between two points:
+# A mathematical expression of a function of a line between two points:
+# f(i) = a*i + b
+
+# a = (y(i+1)-y(i))/(i+1-i)
+# -> a = (y(i+1)-y(i))
+# 
+# point A = y(i) and point B = y=(i+1)
+# y(i) = a*i + b
+# -> b = y(i) - a*i
+# -> b = y(i) - (y(i+1)-y(i))*i
+
+# f(i) = a*i + b
+# -> f(i) = (y(i+1) - y(i))*i + y(i) - (y(i+1) - y(i))*i
+# -> f(i) = y(i)
+
+
 # a = i
 # b = i + 1
 # -> y(x) = f(a) + x*(f(b)-f(a))/(b-a)
+# -> y(i) = f(i) + i*(f(i+1)-f(i))
 
 y = [4.4,2.0,11.0,21.5,7.5]
 t = np.linspace(0, 4, 5)
-
-# part 1.
-
 
 def lin_interpolate(dataset: list[float], floatIndex: float) -> float:
     """Function that finds a float number between entries of a list with linear interpolation
@@ -40,4 +53,4 @@ def lin_interpolate(dataset: list[float], floatIndex: float) -> float:
         return interpolatedNumber
 
 
-print(lin_interpolate(y, 3.1))
+print(lin_interpolate(y, 2.5))
