d847bd1678dffefdd5b1f5f9bb43f1d60ce6ddae
Rekkert <larsrikard2@gmail.com>
2021-09-01 11:40:27 +0200

    changed print statement

--- Chapter 1/circumfrence_and_area.py
+++ Chapter 1/circumfrence_and_area.py
@@ -2,7 +2,7 @@
 
 import math
 
-radius = 3; #asdf
+radius = 3;
 
 def circumfrence (radius: float) -> float:
     return 2*math.pi*radius
@@ -11,4 +11,4 @@ def area (radius: float) -> float:
     return math.pi*radius**2
 
 #String interpolation
-print(f'A circle with a radius of {radius} has a circumfrence of {circumfrence(radius)} and an area of {area(radius)}');
+print(f'A circle with a {radius = } has a circumfrence of {circumfrence(radius)} and an area of {area(radius)}');
