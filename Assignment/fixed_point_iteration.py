a7be6672fce7761e4f54cd10a93e3ae1cecd499e
Rekkert <larsrikard2@gmail.com>
2021-09-10 13:11:57 +0200

    added fixed point iteration

--- Assignment/fixed_point_iteration.py
+++ Assignment/fixed_point_iteration.py
@@ -2,34 +2,56 @@ import math
 import numpy as np
 import matplotlib.pyplot as plot
 
-def newtons_method(x_n: float):
-    x_n_plus_1 = x_n -(custom_function(x_n)/diff_custom_function(x_n))
-    if x_n_plus_1 > 2:
-        x_n_plus_1 = 2
-    elif x_n_plus_1 < -2:
-        x_n_plus_1 = -2
-    return x_n_plus_1
+def newtons_method(x_initial: float, lower_bound: float = -2, upper_bound: float = 2, iterations: int = 10):
+    x = np.zeros(iterations+1)
+    x[0] = x_initial
+    for n in range(iterations):
+        x[n+1] = x[n] - (function(x[n])/diff_function(x[n]))
+        if x[n+1] > upper_bound:
+            x[n+1] = upper_bound
+        elif x[n+1] < lower_bound:
+            x[n+1] = lower_bound
+    return x
 
-def custom_function(x:float) -> float:
+def fixed_point_iteration(x_initial: float = 1, lower_bound: float = -2, upper_bound: float = 2, iterations: int = 10, tolerance_limit: float = 0.05):
+    x_n = np.zeros(iterations+1)
+    x_n[0] = x_initial
+    for i in range (iterations):
+        x_n[i+1] = function_fixed_point_form(x_n[i])
+    print(f'{x_n = }')
+    return x_n
+
+def function_fixed_point_form(x: float) -> float:
+    return (math.e**(-x)-x**3)/2
+
+def function(x: float) -> float:
     return (x**3)+2*x-math.e**(-x)
 
-def diff_custom_function(x:float) -> float:
+def diff_function(x: float) -> float:
     return 3*(x**2)+2+math.e**(-x)
 
-def main():
+def main2():
     iterations = 10
-    x_seek_array = np.zeros(iterations)
-    x_seek_array[0] = 1
 
-    for i in range(iterations-1):
-        x_seek_array[i+1] = newtons_method(x_seek_array[i])
-    print(f'{x_seek_array = }')
+    #root finder points
+    x_seek_array = newtons_method(x_initial=1, iterations=iterations)
+
+    #fixed point points
+    fixed_seek_x = fixed_point_iteration(x_initial=1, iterations=iterations)
+    fixed_seek_y = np.zeros(len(fixed_seek_x))
+    for i in range(len(fixed_seek_y)):
+        fixed_seek_y[i] = function(fixed_seek_x[i])
 
 
-    x_array = np.linspace(-2,2,100)
+    print(f'best fixed value {fixed_seek_x[-1]} = {function(fixed_seek_x[-1])}')
+
+    #x-axis
+    x_array = np.linspace(-2, 2, 100)
+
+    #y-axis
     y_array = np.zeros(100)
     for i in range(len(y_array)):
-        y_array[i] = custom_function(x_array[i])
+        y_array[i] = function(x_array[i])
 
     # Plotting
     plot.close("all")  # Closes all figures before plotting
@@ -37,15 +59,21 @@ def main():
 
     # Plot function in interval
     plot.subplot(2, 1, 1)
+    #function
     plot.plot(x_array, y_array, "b")
-    plot.scatter(x_seek_array,np.zeros(len(x_seek_array)))
+    #root-seeker points
+    #plot.scatter(x_seek_array, np.zeros(len(x_seek_array)), c="r")
+    #Fixed-point seeker points
+    plot.scatter(fixed_seek_x, fixed_seek_y, c="r")
     plot.grid()
     plot.xlabel("x")
     plot.ylabel("f(x)")
 
-
     plot.savefig("plot_heat_tank.svg")
     plot.show()
 
+def main():
+    fixed_point_iteration()
+
 if __name__ == "__main__":
-    main()
+    main2()
